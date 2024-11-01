2e02c4f093d821a96b96b56ef9005b1018f5d159
Karan Purohit <purohitkaran20@gmail.com>
2024-11-01 18:45:52 +0530

    Create taqueria.py

---
+++ taqueria/taqueria.py
@@ -0,0 +1,25 @@
+m = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+    }
+
+j=0
+while True:
+  try:
+      i = (input("Item: ")).title()
+      if i in m:
+        k = float(m[i])
+        j = j + k
+        print('${:.2f}'.format(j))
+
+  except EOFError:
+    break
+  except KeyError:
+   continue
